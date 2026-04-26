a346b2b494cbc2a8378c55bac327de43c3b3f22a
lionel sisso <sissolionel@gmail.com>
2026-04-26 03:10:05 +0100

    added exam submission and commentry

--- server/app/Models/ExamAnswer.php
+++ server/app/Models/ExamAnswer.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class ExamAnswer extends Model
 {
-    //
+    protected $fillable = [
+        'submission_id',
+        'question_id',
+        'answer_text',
+        'selected_option',
+        'file_url',
+        'score'
+    ];
+
+    public function submission()
+    {
+        return $this->belongsTo(ExamSubmission::class, 'submission_id');
+    }
+
+    public function question()
+    {
+        return $this->belongsTo(ExamQuestion::class);
+    }
 }
