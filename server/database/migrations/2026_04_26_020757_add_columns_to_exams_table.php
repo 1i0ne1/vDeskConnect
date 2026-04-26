1bc82e82d6c2ef5b452d3000b20c8acee7711762
lionel sisso <sissolionel@gmail.com>
2026-04-26 03:08:46 +0100

    made exam migration part 2

--- server/database/migrations/2026_04_26_020757_add_columns_to_exams_table.php
+++ server/database/migrations/2026_04_26_020757_add_columns_to_exams_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('exams', function (Blueprint $table) {
-            //
+            $table->boolean('is_ca_test')->default(false)->after('published');
+            $table->integer('week_number')->nullable()->after('is_ca_test');
+            $table->integer('total_marks')->default(0)->after('week_number');
         });
     }
 
@@ -22,7 +24,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('exams', function (Blueprint $table) {
-            //
+            $table->dropColumn(['is_ca_test', 'week_number', 'total_marks']);
         });
     }
 };
