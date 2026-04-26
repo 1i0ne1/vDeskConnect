f5aab17618fed33c890a3a02a37960443f69b6d5
lionel sisso <sissolionel@gmail.com>
2026-04-26 03:48:21 +0100

    added result pin model

--- server/app/Models/ResultPin.php
+++ server/app/Models/ResultPin.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class ResultPin extends Model
 {
-    //
+    protected $fillable = [
+        'school_id',
+        'pin',
+        'student_id',
+        'used',
+        'expires_at',
+    ];
+
+    protected $casts = [
+        'used' => 'boolean',
+        'expires_at' => 'datetime',
+    ];
+
+    public function school()
+    {
+        return $this->belongsTo(School::class);
+    }
+
+    public function student()
+    {
+        return $this->belongsTo(User::class, 'student_id');
+    }
 }
