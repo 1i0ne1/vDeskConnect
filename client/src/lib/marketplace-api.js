e7ccc45b988faae2100ee6d6c210a6b2f8345ed6
lionel sisso <sissolionel@gmail.com>
2026-04-28 20:13:33 +0100

    updated seeder for bulk teacher ,s tudent and staff infinit scroll and filter search tesr

---
+++ client/src/lib/marketplace-api.js
@@ -0,0 +1,16 @@
+import api from './api';
+
+const marketplaceApi = {
+  // Books
+  getBooks: (params = {}) => api.get('/marketplace/books', { params }),
+  addBook: (data) => api.post('/marketplace/books', data),
+  updateBook: (id, data) => api.put(`/marketplace/books/${id}`, data),
+  deleteBook: (id) => api.delete(`/marketplace/books/${id}`),
+
+  // Orders
+  getOrders: (params = {}) => api.get('/marketplace/orders', { params }),
+  placeOrder: (data) => api.post('/marketplace/orders', data),
+  updateOrderStatus: (id, status) => api.put(`/marketplace/orders/${id}/status`, { status }),
+};
+
+export default marketplaceApi;
