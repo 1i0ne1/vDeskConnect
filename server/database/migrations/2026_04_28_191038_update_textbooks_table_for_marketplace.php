a67733c4ef7d17202e21290402b0456c149637ca
lionel sisso <sissolionel@gmail.com>
2026-04-28 20:11:09 +0100

    added market place migrations 2

--- server/database/migrations/2026_04_28_191038_update_textbooks_table_for_marketplace.php
+++ server/database/migrations/2026_04_28_191038_update_textbooks_table_for_marketplace.php
@@ -12,7 +12,17 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('textbooks', function (Blueprint $table) {
-            //
+            $table->foreignId('subject_id')->nullable()->constrained()->onDelete('set null')->after('grade_level_id');
+            $table->boolean('is_electronic')->default(true)->after('price');
+            $table->string('file_url')->nullable()->change();
+            $table->string('physical_form_url')->nullable()->after('file_url');
+            $table->text('description')->nullable()->after('physical_form_url');
+            $table->integer('stock_count')->nullable()->after('description');
+        });
+
+        Schema::table('marketplace_orders', function (Blueprint $table) {
+            $table->foreignId('school_id')->after('id')->constrained()->onDelete('cascade');
+            $table->timestamp('order_date')->useCurrent()->after('payment_ref');
         });
     }
 
